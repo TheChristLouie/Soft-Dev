619fbc7344bb1a205391a342404545d4490b0183
Christopher Louie <clouie50@stuy.edu>
2024-10-01 21:56:20 -0400

    Tried fixing code

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -6,7 +6,6 @@ import random
 from flask import Flask, render_template
 app = Flask(__name__)        
 
-@app.route("/")
 def readfile():
     occupations = {}
     with open('data/occupations.csv') as text:
@@ -21,7 +20,6 @@ def readfile():
             occupations[reader[row+1][0]]=float(reader[row+1][1])
     return occupations
 
-@app.route("/")
 def generateRandom(occupations):
     generated = random.random()*99.8
     for key in occupations.keys():
@@ -31,8 +29,10 @@ def generateRandom(occupations):
     return("error")
 
 @app.route("/wdywtbwygp")
-def page():         
-    return render_template( 'tablified.html', title = "Job Helper", occupation = generateRandom(readfile()), Header = "Table", collection = readfile())
+def page():
+    occupations = readfile()
+    random_occupation = generateRandom(occupations)
+    return render_template('tablified.html', title="Job Helper", occupation=random_occupation, Header="Table", collection=occupations.items())
 
 if __name__ == "__main__":
     app.debug = True
