402dd3d7d98ef6fe251253131e8f45f6be79e7b4
Christopher Louie <clouie50@stuy.edu>
2024-10-09 11:21:18 -0400

    wrote code

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -1,4 +1,4 @@
-# Death Row Coders: Kevin Lin, Raymond Lin, Christopher Louie
+# Clyde 'Thluffy' Sinclair
 # SoftDev
 # October 2024
 
@@ -10,8 +10,8 @@
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
-
-import testmod0
+from flask import session
+import os
 
 #the conventional way:
 #from flask import Flask, render_template, request
@@ -19,59 +19,46 @@ import testmod0
 app = Flask(__name__)    #create Flask object
 
 
-'''
-trioTASK:
-~~~~~~~~~~~ BEFORE RUNNING THIS, ~~~~~~~~~~~~~~~~~~
-...read for understanding all of the code below.
- * Some will work as written;
- *  ...other sections will not. 
-
-TASK:
- Predict which.
- 1. Devise simple tests to isolate components/behaviors.
- 2. Execute your tests.
- 3. Process results.
- 4. Findings yield new ideas for more tests? Yes: do them.
-
-PROTIP: Insert your own in-line comments
- wherever they will help
-  your future self and/or current teammates
-   understand what is going on.
-'''
-
-@app.route("/", methods=['GET'])
+@app.route("/") #, methods=['GET', 'POST'])
 def disp_loginpage():
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app)
-    #print("***DIAG: request obj ***")
-    #print(request)
-    #print("***DIAG: request.args ***")
-    #print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username']) #Breaks Code
-    #print("***DIAG: request.headers ***")
-    #print(request.headers)
-    return render_template( 'login.html' )
-
-
-@app.route("/auth", methods=['GET'])
-def authenticate():
-    print("\n\n\n")
+    ### All of these print statements print to the terminal
+    
+    #print(testmod0.goo())
+    #print("\n\n\n")
     #print("***DIAG: this Flask obj ***")
     #print(app)
     #print("***DIAG: request obj ***")
     #print(request)
     #print("***DIAG: request.args ***")
     #print(request.args)
-    print("***DIAG: request.args['username']  ***")
-    x = request.args['username']
-    print(x)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
     #print("***DIAG: request.headers ***")
     #print(request.headers)
-    return request.args['username']  #response to a form submission
+    request.cookies.get(request.args['username'])
+    #Make new account
+    app.secret_key = os.urandom(32)
+    session[request.args['username']]
+    return render_template( 'login.html' )
 
 
+@app.route("/response.html") # , methods=['GET', 'POST'])
+def responsePage():
+    #print("\n\n\n")
+    #print("***DIAG: this Flask obj ***")
+    #print(app)
+    #print("***DIAG: request obj ***")
+    #print(request)
+    #print("***DIAG: request.args ***")
+    #print(request.args)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    #print("***DIAG: request.headers ***")
+    #print(request.headers)
+    return render_template( 'response.html', username = request.args['username']) #response to a form submission
+
+def logoutpage():
+    return render_template( 'logout.html')
     
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
