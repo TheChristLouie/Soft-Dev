eb2990b36f0187da765b0bce9c90a4b45ce7593b
Christopher Louie <clouie50@stuy.edu>
2024-11-13 11:32:48 -0500

    Made repo

--- 19_db0/build_db.py
+++ 19_db0/build_db.py
@@ -1,4 +1,4 @@
-#Christopher Louie
+#Death Row Coders: Kevin Lin, Raymond Lin, Christopher Louie
 #SoftDev
 #skeleton/stub :: SQLITE3 BASICS
 #Oct 2024
@@ -14,25 +14,37 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 #==========================================================
 
-#c.execute("CREATE TABLE students(name, age, id)")
-command = "INSERT INTO students VALUES"
-with open('students.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        print(row['name'],row['age'],row['id'])
-        command += "(" + row['name'] + ", " + row['age'] + ", " + row['id'] + "),"
-        #c.execute("INSERT INTO students VALUES (" + row['name'] + ", " + row['age'] + ", " + row['id'] + ")")
-command = command[:-1]
-print(command)
+
 """
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 < < < INSERT YOUR TEAM'S DB-POPULATING CODE HERE > > >
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
-#command = ""          # test SQL stmt in sqlite3 shell, save as string
+command = "CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER);"          # test SQL stmt in sqlite3 shell, save as string
 c.execute(command)    # run SQL statement
+command = "CREATE TABLE students(name TEXT, age INTEGER, id INTEGER);"
+c.execute(command)
 
+with open('courses.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        c.execute("INSERT INTO courses(code,mark,id) VALUES (?,?,?)", (row['code'],row['mark'],row['id']))
+
+c.execute('SELECT * FROM courses') #Reads the table
+table = c.fetchall() #Gets the data after reading it
+for tab in table:
+    print(tab)
+
+with open('students.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        c.execute("INSERT INTO students(name,age,id) VALUES (?,?,?)", (row['name'],row['age'],row['id']))
+
+c.execute('SELECT * FROM students') #Reads the table
+table = c.fetchall() #Gets the data after reading it
+for tab in table:
+    print(tab)
 #==========================================================
 
 db.commit() #save changes
